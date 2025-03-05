d9e3052c859a242caf621e9cf462cc95b2e2cd73
NicholasCorniaOrpheus <nicholas.cornia@orpheusinstituut.be>
2025-03-05 13:02:23 +0100

    2025-03-05

---
+++ a_madrigalist_wuerfelspiel/functions.py
@@ -0,0 +1,22 @@
+"""
+Utility functions for the algorithm
+"""
+
+import json, csv
+import random
+import abjad
+
+
+def csv2dict(csv_filename):  # imports a CSV file as dictionary
+    f = open(csv_filename, "r")
+    reader = csv.DictReader(f)
+    d = {"items": []}
+    for row in reader:
+        d["items"].append(row)
+    return d["items"]
+
+
+def json2dict(json_filename):  # imports a JSON file as dictionary
+    with open(json_filename, "r") as f:
+        json_file = json.load(f)
+        return json_file
---
+++ a_madrigalist_wuerfelspiel/main.py
@@ -0,0 +1,7 @@
+"""
+Main code of A Madrigalist Würfelspiel.
+"""
+
+import json, csv
+from "./functions.py" import *
+
